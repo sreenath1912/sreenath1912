a3bc1c45c25ab66998ea9c68a6169064318e1f08
sreenath1912 <you@example.com>
2024-10-23 21:38:12 +0530

    from 16

---
+++ List_methods.py
@@ -0,0 +1,14 @@
+# insert the element at the end of the list
+
+## apend
+
+l = ['xyz', 'abc', 'pqr', 'New_empl']
+l.append('2')
+print(l)  ## o/p, adds to the original list
+
+print(l.append('2'))  ## o/p : none, bcoz it will not give new object
+
+a = 'python'
+print(a.replace('p', 'j'))   #where as string is immutable hence it keeps
+                                         # original and gives the new obj
+
