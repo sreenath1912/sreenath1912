4bcb2776b48229b8a082f5135496f86863d2a6fb
sreenath1912 <you@example.com>
2024-10-23 21:51:33 +0530

    from 16

--- Pandas2.py
+++ Pandas2.py
@@ -1,5 +1,6 @@
 import pandas as pd
-file_path = r"C:\Users\Sreenath_Narayana_re\OneDrive - Dell Technologies\Pictures\sree DS\datasets-20241023T054639Z-001\apple_stocks.csv"
+file_path = (r"C:\Users\Sreenath_Narayana_re\OneDrive - Dell Technologies\Pictures\sree "
+             r"DS\datasets-20241023T054639Z-001\apple_stocks.csv")
 apple_stocks = pd.read_csv(file_path)
 print(apple_stocks.head())
 print()
